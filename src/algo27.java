ec5d467fd385fa1155cbd3f4384452a4c16bf17b
xuyin <1499978024@qq.com>
2018-08-28 19:46:36 +0200

    algo27

---
+++ src/algo27.java
@@ -0,0 +1,17 @@
+public class algo27 {
+    public int removeElement(int[] nums, int val) {
+        int i=0;
+        int pointer=nums.length-1;
+        while(i<=pointer){
+            if(nums[i]==val){
+                nums[i]=nums[pointer];
+                pointer--;
+            }
+            else{
+                i++;
+            }
+        }
+        return pointer+1;
+
+    }
+}
